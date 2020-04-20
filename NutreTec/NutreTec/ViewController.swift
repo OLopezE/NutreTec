ad5ae0270b54ff8825e67c08b63940e3b7ffeb48
Óscar Francisco López Escobar <a01561676@itesm.mx>
2020-04-20 16:03:09 -0600

    esto comienza

---
+++ NutreTec/NutreTec/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NutreTec
+//
+//  Created by Oscar Francisco Lopez Escobar on 20/04/20.
+//  Copyright © 2020 disp_moviles. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
