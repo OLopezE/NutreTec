3d9693f307bc4512a3e23af8f1f94d32952c5a56
Óscar Francisco López Escobar <a01561676@itesm.mx>
2020-04-22 20:28:15 -0600

    mi dia mas completo, falta programacion y mi plan

--- NutreTec/NutreTec/FirstViewController.swift
+++ NutreTec/NutreTec/FirstViewController.swift
@@ -10,11 +10,40 @@ import UIKit
 
 class FirstViewController: UIViewController {
 
+    @IBOutlet weak var fecha: UILabel!
+    var date = Date()
+    
+    @IBOutlet weak var lblCarne: UILabel!
+    @IBOutlet weak var lblVegetales: UILabel!
+    @IBOutlet weak var lblAzucar: UILabel!
+    @IBOutlet weak var lblLeguminosas: UILabel!
+    @IBOutlet weak var lblFrutas: UILabel!
+    @IBOutlet weak var lblCereales: UILabel!
+    @IBOutlet weak var lblGrasas: UILabel!
+    @IBOutlet weak var lblLeche: UILabel!
+    @IBOutlet weak var lblAgua: UILabel!
+    
+    
     override func viewDidLoad() {
+        self.title = "Mi día"
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        obtenDia()
     }
 
-
+    @IBAction func diaMas(_ sender: Any) {
+        date = date.addingTimeInterval(86400)
+        obtenDia()
+    }
+    
+    @IBAction func diaMenos(_ sender: Any) {
+        date = date.addingTimeInterval(-86400)
+        obtenDia()
+    }
+    func obtenDia(){
+        let dia: DateFormatter = DateFormatter()
+        dia.dateFormat = "MMM dd, yyyy"
+        fecha.text = dia.string(from: date)
+    }
 }
 
