98a9f7e32f638305cee89bd0d177e0f54298c30d
Óscar Francisco López Escobar <a01561676@itesm.mx>
2020-05-26 01:22:23 -0600

    Recetarios

--- NutreTec/NutreTec/recetarioViewController.swift
+++ NutreTec/NutreTec/recetarioViewController.swift
@@ -10,13 +10,52 @@ import UIKit
 
 class recetarioViewController: UIViewController {
 
+    @IBOutlet weak var bttnVegetarianos: UIButton!
+    @IBOutlet weak var bttnSmoothies: UIButton!
+    @IBOutlet weak var bttnAntojos: UIButton!
+    @IBOutlet weak var bttnDesayunos: UIButton!
+    @IBOutlet weak var bttnEnsaladas: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        bttnAntojos.layer.cornerRadius = 10
+        bttnDesayunos.layer.cornerRadius = 10
+        bttnEnsaladas.layer.cornerRadius = 10
+        bttnSmoothies.layer.cornerRadius = 10
+        bttnVegetarianos.layer.cornerRadius = 10
         // Do any additional setup after loading the view.
     }
     
-
+    func abreLink (pag : String){
+       if let url = URL(string: pag){
+            if UIApplication.shared.canOpenURL(url){
+                UIApplication.shared.open(url, options: [:])
+                
+            }
+        }
+    }
+    
+    
+    @IBAction func comidas(_ sender: UIButton) {
+        abreLink(pag: "https://drive.google.com/drive/u/1/folders/0AGsVJGgvGEd9Uk9PVA")
+        
+    }
+    
+    
+    @IBAction func ensaladas(_ sender: UIButton) {
+        abreLink(pag: "https://drive.google.com/drive/u/1/folders/0AGsVJGgvGEd9Uk9PVA")
+    }
+    
+    @IBAction func antojos(_ sender: UIButton) {
+        abreLink(pag: "https://drive.google.com/drive/u/1/folders/0AGsVJGgvGEd9Uk9PVA")
+    }
+    @IBAction func smoothies(_ sender: UIButton) {
+        abreLink(pag: "https://drive.google.com/drive/u/1/folders/0AGsVJGgvGEd9Uk9PVA")
+    }
+    
+    @IBAction func Vegetariano(_ sender: UIButton) {
+        abreLink(pag: "https://drive.google.com/drive/u/1/folders/0AGsVJGgvGEd9Uk9PVA")
+    }
     /*
     // MARK: - Navigation
 
