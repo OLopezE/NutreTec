602462b34078492cfbd67857f1299f6ae8fe2afd
Óscar Francisco López Escobar <a01561676@itesm.mx>
2020-05-27 15:38:24 -0600

    constraints en creditos

---
+++ NutreTec/NutreTec/creditosExplicadoViewController.swift
@@ -0,0 +1,60 @@
+//
+//  creditosExplicadoViewController.swift
+//  NutreTec
+//
+//  Created by Oscar Francisco Lopez Escobar on 27/05/20.
+//  Copyright © 2020 disp_moviles. All rights reserved.
+//
+
+import UIKit
+
+class creditosExplicadoViewController: UIViewController {
+    @IBOutlet weak var vwTitle: UIView!
+    @IBOutlet weak var lblNombre: UILabel!
+    @IBOutlet weak var vwBody: UIView!
+    @IBOutlet weak var lblInfo: UILabel!
+    @IBOutlet weak var lblAprendi: UILabel!
+    @IBOutlet weak var imgImagen: UIImageView!
+    @IBOutlet weak var lblTitle: UILabel!
+    @IBOutlet weak var vwFondo: UIView!
+    
+    var colorPrincipal : UIColor!
+    var colorSecundario : UIColor!
+    var nombre = ""
+    var info = ""
+    var aprendi = ""
+    var imagen : UIImage!
+    var colorTexto : UIColor!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        lblNombre.text = nombre
+        lblInfo.text = info
+        lblAprendi.text = aprendi
+        imgImagen.image = imagen
+        vwTitle.backgroundColor = colorPrincipal
+        vwBody.backgroundColor = colorPrincipal
+        view.backgroundColor = colorSecundario
+        lblAprendi.textColor = colorTexto
+        lblInfo.textColor = colorTexto
+        lblNombre.textColor = colorTexto
+        lblTitle.textColor = colorTexto
+        vwFondo.backgroundColor = colorSecundario
+        vwFondo.layer.cornerRadius = 10
+        vwTitle.layer.cornerRadius = 10
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ NutreTec/NutreTec/creditosViewController.swift
@@ -0,0 +1,47 @@
+//
+//  creditosViewController.swift
+//  NutreTec
+//
+//  Created by Oscar Francisco Lopez Escobar on 27/05/20.
+//  Copyright © 2020 disp_moviles. All rights reserved.
+//
+
+import UIKit
+
+class creditosViewController: UIViewController {
+
+    @IBOutlet weak var bttnOscar: UIButton!
+    override func viewDidLoad() {
+        bttnOscar.layer.cornerRadius = 10
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let vista = segue.destination as! creditosExplicadoViewController
+        if segue.identifier == "oscar"{
+            vista.nombre = "Oscar Francisco López Escobar"
+            vista.info = "Correo: oscarfco1998@gmail.com Celular: (614)5138752"
+            vista.aprendi = "Aquellos que no entienden el verdadero dolor no pueden entender la verdadera paz"
+            vista.imagen = UIImage(named: "logoakatsuki")
+            vista.colorPrincipal = UIColor(red: 50/255, green: 50/255, blue: 50/255, alpha: 1)
+            vista.colorSecundario = UIColor(red: 30/255, green: 30/255, blue: 30/255, alpha: 1)
+            vista.colorTexto = UIColor.white
+            
+        }else if segue.identifier == "jesus"{
+            
+        }else if segue.identifier == "mariano"{
+            
+        }
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    
+
+}
